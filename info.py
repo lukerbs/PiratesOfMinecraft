90239014ec381054c2d78d89c0ff33d7da9f35fe
Luke <ldk.kerbs@gmail.com>
2021-08-03 20:39:04 -0700

    get server info

---
+++ info.py
@@ -0,0 +1,23 @@
+from mcstatus import MinecraftServer
+
+file = open('servers.txt', 'r')
+count = 0
+
+ips = []
+while True:
+    count += 1
+    line = file.readline()
+    if not line:
+        break
+    ips.append(line.strip())
+
+for ip in ips:
+    try:
+        server = MinecraftServer.lookup(ip)
+        status = server.status()
+        print(ip)
+        print("Players online:", status.players.online)
+        print("Version:", status.version.name)
+        print('')
+    except:
+        pass
